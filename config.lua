ac5fcce935a79b919dac8e6ef8fbaa572cda4307
Kanoh <93154177+KanohESP@users.noreply.github.com>
2021-11-11 23:41:23 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,18 @@
+Config							= {}
+
+Config.License					= '' -- Dont put nothing (is not cracked)
+
+Config.Locale					= '' -- Próximamente...
+
+Config.respawnlocation = vector3(252.5, -758.2, 30.8)
+Config.spawnlocation1 = vector3(124.9, -879.9, 134.8)
+Config.spawnlocation2 = vector3(78.9, -863.4, 134.8)
+Config.textlocation = vector3(244.9, -742.8, 30.8)
+
+Config.ESXTrigger = 'esx:getSharedObject'
+
+Config.geldamount1 = { label = "Gratis - #1", amount = 0}
+Config.geldamount2 = { label = "100$ - #2", amount = 100}
+Config.geldamount3 = { label = "500$ - #3", amount = 500}
+Config.geldamount4 = { label = "1000$ - #4", amount = 1000}
+Config.geldamount5 = { label = "2500$ - #5", amount = 2500} 
